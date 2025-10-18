19cf7f0b014dcc3d4aff169a84cda911d8daaf14
mhsnayy <muhsinay06@gmail.com>
2025-10-18 06:24:45 +0300

    singelton

--- Design Pattern/Program.cs
+++ Design Pattern/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Design_Patterns
+namespace DesignPatterns
 {
     internal class Program
     {
@@ -15,9 +15,41 @@ namespace Design_Patterns
              * Mesela bir webde kaç kullanıcı aktif sorusunu tüm kullanıcılara ulaştırmak için.
              * Değer değiştiğinde tüm kullanıcılar etkilenir.
              * Singelton design patternde nesnenin ömrü uygulama kapanana kadar devam eder.
-                
-
             */
+
+            var customerManager = CustomerManager.CreateAsSingelton();
+            customerManager.Save();
+
+        }
+
+        public class CustomerManager
+        {
+            private static CustomerManager _customerManager; //Private ve static 
+            static object _lockObject = new object(); // create lock object for thread safety 
+            //çoklu thread ortamında tek bir instance üretmek için kullanılır. Aynı anda 2 thread nesne üretmeye kalkarsa kilitlenir ve sırayla üretir.
+
+            private CustomerManager()
+            {
+
+            }
+            public static CustomerManager CreateAsSingelton() //static method newlenememesi lazım.
+            {
+               lock (_lockObject)
+                {
+                    if (_customerManager == null)
+                    {
+                        _customerManager = new CustomerManager();
+                    }
+                }
+               return _customerManager;
+
+                // return _customerManager ?? (_customerManager = new CustomerManager()); // in one line without lock
+            }
+
+            public void Save()
+            {
+                Console.WriteLine("Saved");
+            }
         }
     }
 }
