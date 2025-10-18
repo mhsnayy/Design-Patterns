f620227df960eddc9995a4bb1510e5ba5cddf2bb
mhsnayy <muhsinay06@gmail.com>
2025-10-18 06:04:38 +0300

    Add project files.

---
+++ Design Pattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Bir nesne örneği sadece 1 kere üretilip, her zaman kullanılması öngörülür. 
+             * Mesela bir webde kaç kullanıcı aktif sorusunu tüm kullanıcılara ulaştırmak için.
+             * Değer değiştiğinde tüm kullanıcılar etkilenir.
+             * Singelton design patternde nesnenin ömrü uygulama kapanana kadar devam eder.
+                
+
+            */
+        }
+    }
+}
